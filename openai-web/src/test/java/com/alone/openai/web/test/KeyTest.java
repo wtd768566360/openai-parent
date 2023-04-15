d0cd643560b648f0b964c455efa17ec929be705a
wutengda <wtd768566360@163.com>
2023-04-15 11:23:46 +0800

    第一次提交

--- openai-web/src/test/java/com/alone/openai/web/test/KeyTest.java
+++
@@ -1,9 +0,0 @@
-package com.alone.openai.web.test;
-
-public class KeyTest {
-
-    public static void main(String[] args) {
-        String a = "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";
-        System.out.println(a.length());
-    }
-}
